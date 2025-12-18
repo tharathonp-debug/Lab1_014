feaf87cff858b22277faadb1212e23134915a06c
tharathonp-debug <tharathon.p@rmutsvmail.com>
2025-12-18 08:44:51 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <body bgcolor="#CC9900">
-	<table width="1238" height="530" border="0"> กกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกกก
+	<table width="1238" height="530" border="0"> 
 		<tr>
 			<td width="23" rowspan="2">&nbsp;</td>
 			<td height="119" colspan="2" bgcolor="#006600">
@@ -31,4 +31,5 @@
 		</tr>
 	</table>
 <body>
+
 
